ddf268020caadf58fa49dc221df2ad3e6924f7ae
QuyNguyenHoang <proemie.2003@gmail.com>
2025-12-15 00:13:03 +0700

    Identity

---
+++ src/CMS.Core/Domain/Identity/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Core.Domain.Identity
+{
+    [Table("AppRoles")]
+    public class AppRole : IdentityRole<Guid>
+    {
+        [Required]
+        [MaxLength(200)]
+        public required string DisplayName { get; set; }
+    }
+}
