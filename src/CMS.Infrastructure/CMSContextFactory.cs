042ed19c8a3de78b77eaf7e2174bd368e6c0523a
QuyNguyenHoang <proemie.2003@gmail.com>
2025-12-15 15:44:44 +0700

    Migrate Database version 1

---
+++ src/CMS.Infrastructure/CMSContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using CMS.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace CMS.Core
+{
+    public class CMSContextFactory : IDesignTimeDbContextFactory<CMS_DBContext>
+    {
+        public CMS_DBContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<CMS_DBContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new CMS_DBContext(builder.Options);
+        }
+    }
+}
